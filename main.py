c1c276aa9519f782f4b7d544770ab33808d16fed
Cheny <49137980+Cheny233@users.noreply.github.com>
2022-03-19 21:37:18 +0800

    update

--- main.py
+++ main.py
@@ -4,31 +4,26 @@ from os import popen
 from PyQt5.QtWidgets import QApplication,QMainWindow
 from PyQt5 import QtCore
 #import Ui_mainWindow
-import Ui_shutWindow1
+import Ui_shutWindow
 
-Times=['8:10','9:0','10:5','10:55','11:45','13:20','14:10','15:0','16:50','21:30']
+Times=['8:10','9:0','10:5','10:55','11:45','14:20','15:10','16:0','16:50','21:30']
 
-'''
-def monitor():
-    global swin
-    tick=localtime()
-    s=str(tick.tm_hour)+':'+str(tick.tm_min)
-    if s in Times and tick.tm_sec==45:
-        swin.show()
-        popen('shutdown -s -t 30')
-    tr=Timer(0.3,monitor)
-    tr.start()
- '''
-
-class shutWindow(QMainWindow,Ui_shutWindow1.Ui_MainWindow):
+class shutWindow(QMainWindow,Ui_shutWindow.Ui_MainWindow):
 
     def __init__(self):
         super().__init__()
+        self.ui=Ui_shutWindow.Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint|QtCore.Qt.WindowMinimizeButtonHint)
         self.timer=QtCore.QTimer()
         self.timer.setInterval(300)
         self.timer.timeout.connect(self.run)
         self.timer.setSingleShot(False)
         self.timer.start()
+    
+    def Quit(self):
+        self.hide()
+        popen('shutdown -a')
     
     def run(self):
         tick=localtime()
@@ -42,15 +37,8 @@ if __name__=='__main__':
 
     # 关闭所有窗口,也不关闭应用程序
     QApplication.setQuitOnLastWindowClosed(False)
-    '''
-    mwin=QMainWindow()
-    mui=Ui_mainWindow.Ui_mainWindow()
-    mui.setupUi(mwin)
-    '''
+
     swin=shutWindow()
-    sui=Ui_shutWindow1.Ui_MainWindow()
-    sui.setupUi(swin)
-    swin.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint|QtCore.Qt.WindowMinimizeButtonHint)
 
     sys.exit(app.exec_())
 
